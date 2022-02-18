85436ca8d907b5b90f891d4ac5df7781a3f98641
Samuel <arias.samuel1010@gmail.com>
2022-02-18 11:05:46 -0300

    Descarga e implementacion de libreria react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import Header from './components/Header.js'
-import Servicios from './components/Servicios.js'
-import SobreNosotros from './components/SobreNosotros.js'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+
+import { store } from './store/store.js'
+import Main from './components/Main.js'
+import Trabajo from './components/Trabajo.js'
+import Footer from './components/Footer.js'
 
 import './App.css'
-import { DivSeparacion, MainContainer } from './styles/components/globalStyles.js'
-import Trabajos from './components/Trabajos.js'
-import Contacto from './components/Contacto.js'
-import Footer from './components/Footer.js'
-import Carrusel from './components/Carrusel.js'
-import { Provider } from 'react-redux'
-import { store } from './store/store.js'
-import Navbar from './UI/Navbar.js'
 
 function App() {
   return (
 
-    <Provider store={store}>
+    <BrowserRouter>
 
-      <Header />
-      <Navbar />
-      <Carrusel />
+      <Provider store={store}>
 
-      <MainContainer>
+        <Routes>
 
-        <DivSeparacion id='sobre-nosotros'/>
-        <SobreNosotros />
-        <DivSeparacion id='servicios'/>
-        <Servicios />
-        <DivSeparacion id='trabajos'/>
-        <Trabajos />
-        <DivSeparacion id='contacto'/>
-        <Contacto />
+          <Route path='/' element={<Main />}/>
+          <Route path='/trabajo' element={<Trabajo />}/>
+          <Route
+            path="*"
+            element={<Navigate to="/" />}
+          />
 
-      </MainContainer>
+        </Routes>
 
-      <Footer />
+        <Footer />
 
-    </Provider>
+      </Provider>
+
+    </BrowserRouter>
     
   )
 }
